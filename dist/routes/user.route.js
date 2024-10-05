8dac96c48d8240e6a61c32a5c2e741dc20563e31
Abhik Patra <theabhik2020@gmail.com>
2024-10-06 01:26:23 +0530

    curd operation

--- dist/routes/user.route.js
+++ dist/routes/user.route.js
@@ -3,9 +3,10 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const router = express.Router();
+//create user
 router.post("/add-user", async (req, res) => {
     console.log("first");
-    const { name, email, password, phoneNumber } = req.body;
+    const { name, email, password, phoneNumber, role, age, isActive } = req.body;
     try {
         // Create a new user with the provided data
         const newUser = await prisma.user.create({
@@ -13,6 +14,9 @@ router.post("/add-user", async (req, res) => {
                 name, // use the values from the request body
                 email,
                 password,
+                role,
+                age,
+                isActive,
                 phoneNumber, // optional if not defined in your schema
             },
         });
@@ -30,5 +34,98 @@ router.post("/add-user", async (req, res) => {
             error: error.message,
         });
     }
+});
+//find unique
+router.get("/get-user/:email", async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: req.params.email,
+        },
+        select: {
+            email: true,
+            name: true,
+        },
+    });
+    res.json({ user });
+});
+// find all user
+router.get("/all-user", async (req, res) => {
+    const user = await prisma.user.findMany();
+    res.json({ user });
+});
+//findFirst
+router.get("/find-first", async (req, res) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            age: {
+                gt: 50,
+            },
+        },
+        orderBy: {
+            id: "desc",
+        },
+    });
+    res.json({ user });
+});
+//find Many
+router.get("/find-many", async (req, res) => {
+    const user = await prisma.user.findMany({
+        where: {
+            email: {
+                endsWith: ".in",
+            },
+        },
+        orderBy: {
+            id: "desc",
+        },
+    });
+    res.json({ user });
+});
+//update user
+router.put("/update-user/:email", async (req, res) => {
+    const updateUser = await prisma.user.update({
+        where: {
+            email: "emma.thompson@site.uk",
+        },
+        data: {
+            name: "Viola the Magnificent",
+        },
+    });
+    res.json({ updateUser });
+});
+//update many
+router.put("/update-many/:email", async (req, res) => {
+    const updateUser = await prisma.user.updateMany({
+        where: {
+            email: {
+                contains: ".in",
+            },
+        },
+        data: {
+            role: "moderator",
+        },
+    });
+    res.json({ updateUser });
+});
+//update upsert
+router.put("/update-upsert/:email", async (req, res) => {
+    const updateUser = await prisma.user.upsert({
+        where: {
+            email: "viola@prisma.io",
+        },
+        update: {
+            name: "Viola the Magnificent",
+        },
+        create: {
+            email: "viola@prisma.io",
+            name: "viola",
+            phoneNumber: "1234567890",
+            password: "hashed_password_1",
+            role: "admin",
+            age: 45,
+            isActive: false,
+        },
+    });
+    res.json({ updateUser });
 });
 export default router;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,20 +5,22 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const router: Router = express.Router();
 
+//create user
 router.post("/add-user", async (req: Request, res: Response) => {
-    console.log("first")
-  const { name, email, password, phoneNumber } = req.body;
-
-
+  console.log("first");
+  const { name, email, password, phoneNumber, role, age, isActive } = req.body;
 
   try {
     // Create a new user with the provided data
     const newUser = await prisma.user.create({
       data: {
-        name,          // use the values from the request body
+        name, // use the values from the request body
         email,
         password,
-        phoneNumber,   // optional if not defined in your schema
+        role,
+        age,
+        isActive,
+        phoneNumber, // optional if not defined in your schema
       },
     });
 
@@ -35,6 +37,108 @@ router.post("/add-user", async (req: Request, res: Response) => {
       error: error.message,
     });
   }
+});
+
+//find unique
+router.get("/get-user/:email", async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: req.params.email,
+    },
+    select: {
+      email: true,
+      name: true,
+    },
+  });
+  res.json({ user });
+});
+
+// find all user
+router.get("/all-user", async (req, res) => {
+  const user = await prisma.user.findMany();
+  res.json({ user });
+});
+
+//findFirst
+
+router.get("/find-first", async (req, res) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      age: {
+        gt: 50,
+      },
+    },
+    orderBy: {
+      id: "desc",
+    },
+  });
+  res.json({ user });
+});
+
+//find Many
+
+router.get("/find-many", async (req, res) => {
+  const user = await prisma.user.findMany({
+    where: {
+      email: {
+        endsWith: ".in",
+      },
+    },
+    orderBy: {
+      id: "desc",
+    },
+  });
+  res.json({ user });
+});
+
+//update user
+router.put("/update-user/:email", async (req, res) => {
+  const updateUser = await prisma.user.update({
+    where: {
+      email: "emma.thompson@site.uk",
+    },
+    data: {
+      name: "Viola the Magnificent",
+    },
+  });
+  res.json({ updateUser });
+});
+
+//update many
+router.put("/update-many/:email", async (req, res) => {
+  const updateUser = await prisma.user.updateMany({
+    where: {
+      email: {
+        contains: ".in",
+      },
+    },
+    data: {
+      role: "moderator",
+    },
+  });
+  res.json({ updateUser });
+});
+
+//update upsert
+router.put("/update-upsert/:email", async (req, res) => {
+  const updateUser = await prisma.user.upsert({
+    where: {
+      email: "viola@prisma.io",
+    },
+    update: {
+      name: "Viola the Magnificent",
+    },
+    create: {
+      email: "viola@prisma.io",
+      name: "viola",
+      phoneNumber: "1234567890",
+      password: "hashed_password_1",
+      role: "admin",
+      age: 45,
+      isActive: false,
+    },
+  });
+  res.json({ updateUser });
 });
 
 export default router;
